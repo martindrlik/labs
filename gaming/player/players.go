293c38ac7968745023b6ef6cbd3d2fc617895025
Martin Drlík <533091+martindrlik@users.noreply.github.com>
2020-11-21 21:08:55 +0100

    gaming/player: rename

--- gaming/player/players.go
+++ gaming/player/players.go
@@ -1,22 +1,22 @@
 package player
 
 type Players struct {
-	data map[string]chan string
+	online map[string]chan string
 }
 
 func (ps *Players) Register(players map[string]chan string) {
-	newSize := len(ps.data) + len(players)
+	newSize := len(ps.online) + len(players)
 	newData := make(map[string]chan string, newSize)
-	for k, v := range ps.data {
+	for k, v := range ps.online {
 		newData[k] = v
 	}
 	for k, v := range players {
 		newData[k] = v
 	}
-	ps.data = newData
+	ps.online = newData
 }
 
 func (ps *Players) Player(name string) (ch chan string, ok bool) {
-	ch, ok = ps.data[name]
+	ch, ok = ps.online[name]
 	return
 }
